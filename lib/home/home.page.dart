4a91d3de439c1ef7118b05ec7a8ed79669e94d01
sajivddin <sajivddin.websolutions@gmail.com>
2025-04-29 18:55:29 +0530

    change

--- lib/home/home.page.dart
+++ lib/home/home.page.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_options.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_slider/carousel_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,126 +12,133 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> imageList = [
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height / 2,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0xFFECD7FD), Color(0xFFF5F2F7)],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Color(0xFFECD7FD), Color(0xFFF5F2F7)],
+                    ),
                   ),
                 ),
-              ),
-              Image.asset("assets/bgimage.png"),
-              Positioned(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 54.h),
-                    Row(
-                      children: [
-                        SizedBox(width: 20.w),
-                        Container(
-                          width: 50.w,
+                Image.asset("assets/bgimage.png"),
+                Positioned(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 54.h),
+                      Row(
+                        children: [
+                          SizedBox(width: 20.w),
+                          Container(
+                            width: 50.w,
+                            height: 50.h,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFFFFFFF),
+                            ),
+                            child: Icon(
+                              Icons.location_on,
+                              color: Color(0xFF891AFF),
+                            ),
+                          ),
+                          SizedBox(width: 6.w),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Your Location",
+                                style: GoogleFonts.dmSans(
+                                  fontSize: 13.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(255, 97, 91, 104),
+                                ),
+                              ),
+                              Text(
+                                "Jaipur, rajasthan",
+                                style: GoogleFonts.dmSans(
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(255, 137, 26, 255),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            width: 50.w,
+                            height: 50.h,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: Center(child: Icon(Icons.favorite_border)),
+                          ),
+                          SizedBox(width: 8.w),
+                          Container(
+                            width: 50.w,
+                            height: 50.h,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: Center(
+                              child: Icon(Icons.notifications_none),
+                            ),
+                          ),
+                          SizedBox(width: 20.w),
+                        ],
+                      ),
+                      SizedBox(height: 16.h),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                        child: SizedBox(
                           height: 50.h,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color(0xFFFFFFFF),
-                          ),
-                          child: Icon(
-                            Icons.location_on,
-                            color: Color(0xFF891AFF),
-                          ),
-                        ),
-                        SizedBox(width: 6.w),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Your Location",
-                              style: GoogleFonts.dmSans(
-                                fontSize: 13.sp,
-                                fontWeight: FontWeight.w500,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(
+                                top: 8.h,
+                                right: 20.r,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              prefixIcon: Icon(Icons.search),
+                              hintText: "Search Anything...",
+                              hintStyle: GoogleFonts.dmSans(
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400,
                                 color: Color.fromARGB(255, 97, 91, 104),
                               ),
-                            ),
-                            Text(
-                              "Jaipur, rajasthan",
-                              style: GoogleFonts.dmSans(
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.w500,
-                                color: Color.fromARGB(255, 137, 26, 255),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40.r),
+                                borderSide: BorderSide.none,
                               ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                        Container(
-                          width: 50.w,
-                          height: 50.h,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          child: Center(child: Icon(Icons.favorite_border)),
-                        ),
-                        SizedBox(width: 8.w),
-                        Container(
-                          width: 50.w,
-                          height: 50.h,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          child: Center(child: Icon(Icons.notifications_none)),
-                        ),
-                        SizedBox(width: 20.w),
-                      ],
-                    ),
-                    SizedBox(height: 16.h),
-                    Padding(
-                      padding: EdgeInsets.only(left: 20.w, right: 20.w),
-                      child: SizedBox(
-                        height: 50.h,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(
-                              top: 8.h,
-                              right: 20.r,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            prefixIcon: Icon(Icons.search),
-                            hintText: "Search Anything...",
-                            hintStyle: GoogleFonts.dmSans(
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w400,
-                              color: Color.fromARGB(255, 97, 91, 104),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40.r),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40.r),
-                              borderSide: BorderSide.none,
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40.r),
+                                borderSide: BorderSide.none,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 16.h),
-                    SizedBox(
-                      child: Padding(
+                      SizedBox(height: 16.h),
+                      Padding(
                         padding: EdgeInsets.only(left: 20.w, right: 20.w),
                         child: Container(
                           width: MediaQuery.of(context).size.width,
@@ -169,13 +178,34 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 24.h),
+                      SizedBox(
+                        height: 180.h,
+                        child: CarouselSlider(
+                          scrollPhysics: NeverScrollableScrollPhysics(),
+                          slideTransform: DefaultTransform(),
+                          autoSliderTransitionTime: Duration(seconds: 1),
+                          autoSliderDelay: Duration(seconds: 2),
+                          enableAutoSlider: true,
+                          //slideIndicator: CircularSlideIndicator(),
+                          unlimitedMode: true,
+                          children: [
+                            Image.asset('assets/frame.png'),
+                            Image.asset('assets/frame1.png'),
+                            Image.asset('assets/frame2.png'),
+                            Image.asset("assets/frame3.png"),
+                            Image.asset("assets/frame4.png"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            Text("asdfafdafa  dfhakfjdh adkhfak"),
+          ],
+        ),
       ),
     );
   }
