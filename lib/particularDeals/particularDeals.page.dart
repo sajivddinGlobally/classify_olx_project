a79301245446c0fe9cc24bba380d5bdc1d0dba73
sajivddin <sajivddin.websolutions@gmail.com>
2025-05-28 17:08:24 +0530

    change

--- lib/particularDeals/particularDeals.page.dart
+++ lib/particularDeals/particularDeals.page.dart
@@ -88,95 +88,37 @@ class _ParticularDealsPageState extends ConsumerState<ParticularDealsPage> {
                     ),
                   ],
                 ),
-                // Padding(
-                //   padding: EdgeInsets.only(left: 20.w, top: 20.h),
-                //   child: Row(
-                //     children: [
-                //       Container(
-                //         // width: 135.w,
-                //         height: 26.h,
-                //         decoration: BoxDecoration(
-                //           borderRadius: BorderRadius.circular(30.r),
-                //           color: Color.fromARGB(25, 137, 26, 255),
-                //         ),
-                //         child: Padding(
-                //           padding: EdgeInsets.only(left: 6.w, right: 6.w),
-                //           child: Row(
-                //             children: [
-                //               Icon(
-                //                 Icons.location_on,
-                //                 size: 15.sp,
-                //                 color: Color.fromARGB(255, 137, 26, 255),
-                //               ),
-                //               Text(
-                //                 "Udaipur, rajasthan",
-                //                 style: GoogleFonts.dmSans(
-                //                   fontSize: 12.sp,
-                //                   fontWeight: FontWeight.w500,
-                //                   color: Color.fromARGB(255, 137, 26, 255),
-                //                 ),
-                //               ),
-                //             ],
-                //           ),
-                //         ),
-                //       ),
-                //       Spacer(),
-                //       Container(
-                //         width: 30.w,
-                //         height: 30.h,
-                //         decoration: BoxDecoration(
-                //           color: Color.fromARGB(25, 137, 26, 255),
-                //           shape: BoxShape.circle,
-                //         ),
-                //         child: Center(
-                //           child: Icon(
-                //             Icons.favorite_border,
-                //             size: 15.sp,
-                //             color: Color.fromARGB(255, 137, 26, 255),
-                //           ),
-                //         ),
-                //       ),
-                //       SizedBox(width: 20.w),
-                //     ],
-                //   ),
-                // ),
                 Padding(
                   padding: EdgeInsets.only(left: 20.w, top: 20.h),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            //"Nike Air Jorden 55 Medium",
-                            particular.data.name,
-                            style: GoogleFonts.dmSans(
-                              fontSize: 14.sp,
-                              fontWeight: FontWeight.w500,
-                              color: Color.fromARGB(255, 97, 91, 104),
-                            ),
+                      Container(
+                        // width: 135.w,
+                        height: 26.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30.r),
+                          color: Color.fromARGB(25, 137, 26, 255),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 6.w, right: 6.w),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                size: 15.sp,
+                                color: Color.fromARGB(255, 137, 26, 255),
+                              ),
+                              Text(
+                                "Udaipur, rajasthan",
+                                style: GoogleFonts.dmSans(
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(255, 137, 26, 255),
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            //"\$450.00",
-                            particular.data.price.toString(),
-                            style: GoogleFonts.dmSans(
-                              fontSize: 18.sp,
-                              fontWeight: FontWeight.w600,
-                              color: Color.fromARGB(255, 137, 26, 255),
-                            ),
-                          ),
-                          SizedBox(height: 30.h),
-                          Text(
-                            // "Product Specification ",
-                            particular.data.category,
-                            style: GoogleFonts.dmSans(
-                              fontSize: 20.sp,
-                              fontWeight: FontWeight.w600,
-                              color: Color.fromARGB(255, 36, 33, 38),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                       Spacer(),
                       Container(
@@ -195,6 +137,42 @@ class _ParticularDealsPageState extends ConsumerState<ParticularDealsPage> {
                         ),
                       ),
                       SizedBox(width: 20.w),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        //"Nike Air Jorden 55 Medium",
+                        particular.data.name,
+                        style: GoogleFonts.dmSans(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromARGB(255, 97, 91, 104),
+                        ),
+                      ),
+                      Text(
+                        //"\$450.00",
+                        particular.data.price.toString(),
+                        style: GoogleFonts.dmSans(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 137, 26, 255),
+                        ),
+                      ),
+                      SizedBox(height: 30.h),
+                      Text(
+                        // "Product Specification ",
+                        particular.data.category,
+                        style: GoogleFonts.dmSans(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 36, 33, 38),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -239,6 +217,7 @@ class _ParticularDealsPageState extends ConsumerState<ParticularDealsPage> {
                       borderRadius: BorderRadius.circular(20.r),
                     ),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Padding(
                           padding: EdgeInsets.only(left: 16.w, top: 16.h),
@@ -265,9 +244,10 @@ class _ParticularDealsPageState extends ConsumerState<ParticularDealsPage> {
                                       color: Color.fromARGB(255, 97, 91, 104),
                                     ),
                                   ),
-                                  SizedBox(height: 5.h),
+                                  // SizedBox(height: 5.h),
                                   Text(
-                                    "+91-9812456578    |    Jaipur",
+                                    // "+91-9812456578    | "
+                                    "Jaipur",
                                     style: GoogleFonts.dmSans(
                                       fontSize: 14.sp,
                                       fontWeight: FontWeight.w500,
@@ -296,29 +276,29 @@ class _ParticularDealsPageState extends ConsumerState<ParticularDealsPage> {
                           ),
                         ),
                         SizedBox(height: 20.h),
-                        Padding(
-                          padding: EdgeInsets.only(left: 20.w, right: 20.r),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 49.h,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(35.45.r),
-                              border: Border.all(
-                                color: Color.fromARGB(255, 137, 26, 255),
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Message me",
-                                style: GoogleFonts.dmSans(
-                                  fontSize: 15.sp,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color.fromARGB(255, 137, 26, 255),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
+                        // Padding(
+                        //   padding: EdgeInsets.only(left: 20.w, right: 20.r),
+                        //   child: Container(
+                        //     width: MediaQuery.of(context).size.width,
+                        //     height: 49.h,
+                        //     decoration: BoxDecoration(
+                        //       borderRadius: BorderRadius.circular(35.45.r),
+                        //       border: Border.all(
+                        //         color: Color.fromARGB(255, 137, 26, 255),
+                        //       ),
+                        //     ),
+                        //     child: Center(
+                        //       child: Text(
+                        //         "Message me",
+                        //         style: GoogleFonts.dmSans(
+                        //           fontSize: 15.sp,
+                        //           fontWeight: FontWeight.w500,
+                        //           color: Color.fromARGB(255, 137, 26, 255),
+                        //         ),
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
                       ],
                     ),
                   ),
