527478e3e78a3917ebba84580a8b6e0d8286a508
sajivddin <sajivddin.websolutions@gmail.com>
2025-06-14 18:46:21 +0530

    change

--- lib/cagetory/sale.property.form.page.dart
+++ lib/cagetory/sale.property.form.page.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shopping_app_olx/cagetory/car.form.page.dart';
 import 'package:shopping_app_olx/cagetory/new.plan.page.dart';
 import 'package:shopping_app_olx/config/pretty.dio.dart';
+import 'package:shopping_app_olx/home/home.page.dart';
 import 'package:shopping_app_olx/new/new.service.dart';
 
 class SalePropertyFormPage extends StatefulWidget {
@@ -32,8 +42,65 @@ class _SalePropertyFormPageState extends State<SalePropertyFormPage> {
   final titleControler = TextEditingController();
   final desContrler = TextEditingController();
 
+  File? image;
+  final picker = ImagePicker();
+
+  Future<void> pickImageFormCamera() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      final PickedFile = await picker.pickImage(source: ImageSource.camera);
+      if (PickedFile != null) {
+        setState(() {
+          image = File(PickedFile.path);
+        });
+      }
+    } else {
+      print("Camera Permission isdenied");
+    }
+  }
+
+  Future<void> pickImageFromGallery() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      final PickedFile = await picker.pickImage(source: ImageSource.gallery);
+      if (PickedFile != null) {
+        setState(() {
+          image = File(PickedFile.path);
+        });
+      }
+    } else {
+      print("Gallery Permission isdenied");
+    }
+  }
+
+  Future showImage() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder:
+          (context) => CupertinoActionSheet(
+            actions: [
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  pickImageFormCamera();
+                  Navigator.pop(context);
+                },
+                child: Text("Camera"),
+              ),
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  pickImageFromGallery();
+                  Navigator.pop(context);
+                },
+                child: Text("Gallery"),
+              ),
+            ],
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    var box = Hive.box("data");
     Map<String, dynamic> data = {
       "type": typeContrller.text,
       "bhk": bhkController.text,
@@ -184,6 +251,39 @@ class _SalePropertyFormPageState extends State<SalePropertyFormPage> {
                         maxlenghts: 4096,
                       ),
                       SizedBox(height: 20.h),
+                      GestureDetector(
+                        onTap: () {
+                          showImage();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 216.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.r),
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey, width: 1.w),
+                          ),
+                          child:
+                              image == null
+                                  ? Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(Icons.upload),
+                                      Text("Upload Image"),
+                                    ],
+                                  )
+                                  : ClipRRect(
+                                    borderRadius: BorderRadius.circular(15.r),
+                                    child: Image.file(
+                                      image!,
+                                      width: MediaQuery.of(context).size.width,
+                                      height: 216.h,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                        ),
+                      ),
+                      SizedBox(height: 20.h),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           minimumSize: Size(
@@ -193,14 +293,49 @@ class _SalePropertyFormPageState extends State<SalePropertyFormPage> {
                           backgroundColor: Color.fromARGB(255, 137, 26, 255),
                         ),
                         onPressed: () async {
-                          final apiserce = APIService(await createDio());
-                          await apiserce.addProduct(data);
-                          Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                              builder: (context) => NewPlanPage(),
-                            ),
-                          );
+                          try {
+                            final apiserce = APIService(await createDio());
+                            await apiserce.addProduct({
+                              "category": "text",
+                              "user_id": "${box.get("id")}",
+                              "image": await MultipartFile.fromFile(
+                                image!.path,
+                                filename: image!.path.split('/').last,
+                              ),
+                              "json_data": jsonEncode({
+                                "type": typeContrller.text,
+                                "bhk": bhkController.text,
+                                "bathroo": bathroomController.text,
+                                "frun": furshingController.text,
+                                "project": projectControler.text,
+                                "listed": listedControlelr.text,
+                                "super": superbuildController.text,
+                                "car": carpetControlelr.text,
+                                "main": mentationController.text,
+                                "flor": florControlelr.text,
+                                "flornumber": florNumberControler.text,
+                                "carparking": carparkingContrller.text,
+                                "facing": facingcontroler.text,
+                                "projectname": proejctnameControler.text,
+                                "title": titleControler.text,
+                                "des": desContrler.text,
+                              }),
+                            });
+                            Fluttertoast.showToast(
+                              msg: "Product Add Successfull",
+                            );
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                              (route) => false,
+                            );
+                          } catch (e) {
+                            log(e.toString());
+
+                            Fluttertoast.showToast(msg: "Product Add Failed");
+                          }
                         },
                         child: Text(
                           "Continue",
