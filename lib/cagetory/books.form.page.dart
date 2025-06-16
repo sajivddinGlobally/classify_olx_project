24ffdefd3710d857a9b4d4a189370ae0231b60f3
sajivddin <sajivddin.websolutions@gmail.com>
2025-06-16 11:15:23 +0530

    change

--- lib/cagetory/books.form.page.dart
+++ lib/cagetory/books.form.page.dart
@@ -237,7 +237,7 @@ class _BooksFormPageState extends State<BooksFormPage> {
                               });
                               final apiserce = APIService(await createDio());
                               await apiserce.addProduct({
-                                "category": "text",
+                                "category": "test",
                                 "user_id": "${box.get("id")}",
                                 "image": await MultipartFile.fromFile(
                                   image!.path,
